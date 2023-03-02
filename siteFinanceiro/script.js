f1f5508c61d87ba8891c91c96872d45f38040f84
Giuliano Nolasco <110136470+GiulianoNolasco@users.noreply.github.com>
2023-03-02 11:49:41 -0300

    commit site financas

---
+++ siteFinanceiro/script.js
@@ -0,0 +1,6 @@
+MostrarLancarDespesa = () => {
+  let informacoesLancarDespesa = document.querySelector(
+    "#informacoesLancarDespesa"
+  );
+  informacoesLancarDespesa.classList.toggle("esconder");
+};
